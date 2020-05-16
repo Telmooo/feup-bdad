eb73d50c8f84664410d70893529dd2dcb2fe5ead
Telmooo <telmoabaptista@hotmail.com>
2020-05-16 17:53:32 +0100

    changed column name

--- sql/int2.sql
+++ sql/int2.sql
@@ -2,8 +2,8 @@
 .headers on
 .nullvalue NULL
 
-SELECT DiaSemana, printf("%.2f", SUM(strftime('%s', HoraFim) - strftime('%s', HoraInicio))/ COUNT(*) / 3600.0) as Avg
+SELECT DiaSemana, printf("%.2f", SUM(strftime('%s', HoraFim) - strftime('%s', HoraInicio))/ COUNT(*) / 3600.0) as 'Average Work Hours'
     FROM ((HorarioTrabalho NATURAL JOIN Medico) NATURAL JOIN Horario)
         GROUP BY DiaSemana
 UNION
-SELECT DiaSemana, '0.00' as Avg FROM Horario WHERE DiaSemana NOT IN (SELECT DISTINCT DiaSemana FROM ((HorarioTrabalho NATURAL JOIN Medico) NATURAL JOIN Horario));
+SELECT DiaSemana, '0.00' as 'Average Work Hours' FROM Horario WHERE DiaSemana NOT IN (SELECT DISTINCT DiaSemana FROM ((HorarioTrabalho NATURAL JOIN Medico) NATURAL JOIN Horario));
