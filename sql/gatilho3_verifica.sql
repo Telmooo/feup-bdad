799c0b8a33683bcee0e94a64dbbb9f703bf9ee47
Iohan Sardinha <ioxsardinha@gmail.com>
2020-05-23 16:57:04 +0100

    um médico não pode ter duas consultas na mesma hora de início

---
+++ sql/gatilho3_verifica.sql
@@ -0,0 +1,14 @@
+/* um médico não pode ter duas consultas na mesma hora de início*/
+
+.mode columns
+.headers on
+.nullvalue NULL
+
+
+UPDATE Evento SET Data =  "2012-01-22 10:11:38" WHERE EventoID = 1865;
+
+DELETE FROM Consulta WHERE EventoID = 9999;
+DELETE FROM Evento WHERE EventoID = 9999;
+INSERT INTO Evento(EventoID,Descricao, Data, Admissao, Quarto) VALUES (9999, "descricao","2012-01-22 10:11:38" ,154, 238);
+INSERT INTO Consulta( EventoID, Diagnostico, Medico) VALUES (9999,"Porphyria",389);
+
