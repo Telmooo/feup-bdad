cbc73511a907c9b66fb43a9cd0f81d15f6b2be74
Iohan Sardinha <ioxsardinha@gmail.com>
2020-05-23 10:56:20 +0100

    Quem são os médicos mais velhos de cada departamento?

---
+++ sql/int8.sql
@@ -0,0 +1,14 @@
+/*Quem são os médicos mais velhos de cada departamento?*/
+
+.mode columns
+.headers on
+.nullvalue NULL
+
+SELECT Pessoa.PessoaID, Pessoa.Nome, MIN(Pessoa.DataNascimento) AS DataNascimento, Departamento.Nome AS Departamento 
+FROM Pessoa
+INNER JOIN Medico ON Pessoa.PessoaID = Medico.StaffID
+INNER JOIN Staff ON Pessoa.PessoaID = Staff.PessoaID
+INNER JOIN Especializacao ON Staff.Especializacao = Especializacao.EspecializacaoID
+INNER JOIN Departamento ON Especializacao.Departamento = Departamento.NumIdentificador
+GROUP BY Departamento.Nome
+ORDER BY Pessoa.Nome; 
