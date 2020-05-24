80f0683f9a4f1309b04837ec9784d84ca5d1fd9b
Henrique Pereira <46387500+GodAnri@users.noreply.github.com>
2020-05-24 23:34:52 +0100

    Updated indentation on int7.sql

--- sql/int7.sql
+++ sql/int7.sql
@@ -6,18 +6,8 @@ SELECT DISTINCT Nome, NomeResponsavel, NomeDepartamento
     FROM ((SELECT NomeDepartamento, Paciente, Nome AS NomeResponsavel
         FROM (((SELECT Departamento.Nome AS NomeDepartamento, Admissao, Medico
             FROM ((SELECT *
-                FROM (Evento
-NATURAL JOIN
-                Consulta))
-JOIN
-            Departamento)
-                WHERE Medico=Responsavel)
-JOIN
-        Pessoa)
-JOIN
-        Admissao)
-            WHERE AdmissaoID=Admissao AND Medico=PessoaID)
-JOIN
-    Pessoa)
+                FROM (Evento NATURAL JOIN Consulta)) JOIN Departamento)
+                WHERE Medico=Responsavel) JOIN Pessoa) JOIN Admissao)
+            WHERE AdmissaoID=Admissao AND Medico=PessoaID) JOIN Pessoa)
         WHERE Paciente=PessoaID
             ORDER BY Nome, NomeDepartamento;
